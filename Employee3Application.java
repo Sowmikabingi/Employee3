2361ff98f4bb81d3f7b87ee6e7cf5a22f2f061dd
Sowmikabingi <136006004+Sowmikabingi@users.noreply.github.com>
2023-06-13 14:01:38 -0500

    Add files via upload

---
+++ Employee3Application.java
@@ -0,0 +1,14 @@
+package com.example.Employee3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Employee3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Employee3Application.class, args);
+		System.out.println("Hi, I am Sowmika, How are you all");
+	}
+
+}
